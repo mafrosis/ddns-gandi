11b5d54ebf8eebab0e483994680a163d7b4cf3ac
Matt Black <github@mafro.net>
2021-09-26 15:09:00 +1000

    Scripted subdomain, domain, pppoe0

--- ddns-gandi.sh
+++ ddns-gandi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This file: /etc/dhcp/dhclient-exit-hooks.d/ddns-gandi.sh
 
-/config/scripts/ddns-gandi -host myrouter -domain example.com -ifname eth0 -apiKey XXXXX >> /var/log/ddns-gandi.log 2>&1
+/config/scripts/ddns-gandi -host home -domain mafro.net -ifname pppoe0 -apiKey ${GANDI_API_TOKEN} >> /var/log/ddns-gandi.log 2>&1
 
 logger ${0##*/}: 'ddns-gandi attempted to update DNS records'
